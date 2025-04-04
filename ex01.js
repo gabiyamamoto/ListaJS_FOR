cf0a79d0e456a0324221772f9545f3b9fcb672fc
Gabi Emi <gabiemi2021@gmail.com>
2025-04-04 09:58:03 -0300

    ex01 - Imprimir números de 1 a 50

--- ex01.js
+++ ex01.js
@@ -1,6 +1,3 @@
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/gabiyamamoto/ListaJS_FOR.git
-git push -u origin main
+for (let i = 1; i <= 50; i++) {
+    console.log (i);
+}
