1c56ab088f78b78d6a48fe9b0bb4703a88a6e709
gabiyamamoto <gabiemi2021@gmail.com>
2025-04-04 21:53:17 -0300

    ex08 - Calcular o fatorial de um número (N = 6)

--- ex08.js
+++ ex08.js
@@ -6,4 +6,4 @@ for (let i = 1; i <= 50; i++) {
     }
 }
 
-console.log (`A quantidade de números divisíveis por 3 entre 1 e 50 é: ${qntNumeros}`);
+console.log (`A quantidade de números divisíveis por 3 entre 1 e 50 é: ${qntNumeros}`)
