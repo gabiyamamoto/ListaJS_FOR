6d3f756ed96698140fa6462bd4e39c9cd9f75f39
gabiyamamoto <gabiemi2021@gmail.com>
2025-04-04 21:52:01 -0300

    Contar quantos números divisíveis por 3 existem entre 1 e N (N = 50)

--- ex08.js
+++ ex08.js
@@ -1,3 +1,9 @@
+let qntNumeros = 0;
+
 for (let i = 1; i <= 50; i++) {
-    if (i % 3 == 0) 
+    if (i % 3 == 0) {
+        qntNumeros++
+    }
 }
+
+console.log (`A quantidade de números divisíveis por 3 entre 1 e 50 é: ${qntNumeros}`);
